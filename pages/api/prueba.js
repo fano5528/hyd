ccbb64081f38f6f13d8e498d68c8e8dfef5c742b
ops@fanodesign.com <josefano@MacBook-Air-96.local>
2023-03-24 20:54:20 -0600

    a

--- pages/api/prueba.js
+++ pages/api/prueba.js
@@ -1,11 +1,23 @@
-import { dbConnect } from '@/lib/dbConnect';
-import Order from '@/models/Order';
-import Product from '@/models/Product';
-
-dbConnect();
+const Stripe = require('stripe');
+const stripe = Stripe(process.env.STRIPE_KEY);
 
 export default async function handler(req,res) {
-    const hola = await Product.findOne({id: "price_1Mp388J8K7osZc8AWKgqecR6"});
-    console.log(hola);
-    res.status(200).json({message: 'ok'})
+    const body = req.body
+    let cart = []
+    for (let i = 0; i < body.length; i++) {
+        cart.push({price: eval(`body.id${i}`), quantity: eval(`body.quantity${i}`)})
+    }
+    const session = await stripe.checkout.sessions.create({
+        success_url: 'https:/hydronaut.mx/api/gracias?session_id={CHECKOUT_SESSION_ID}',
+        cancel_url: 'https://hydronaut.mx/carrito',
+        line_items: cart,
+        mode: 'payment',
+        shipping_address_collection: {
+            allowed_countries: ['MX'],
+        },
+        phone_number_collection: {
+            enabled: true,
+        },
+    });
+    res.redirect(303, session.url);
 }
