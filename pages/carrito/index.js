99259ea161463d617ced9cdcae910a0f351550c4
ops@fanodesign.com <josefano@MacBook-Air-96.local>
2023-03-24 20:40:46 -0600

    a

--- pages/carrito/index.js
+++ pages/carrito/index.js
@@ -112,7 +112,7 @@ export default function Carrito({categories, productsFetched}) {
         <div className="bg-ow">
       <div className="mx-auto max-w-2xl px-4 pt-16 pb-24 sm:px-6 lg:max-w-7xl lg:px-8">
         <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Carrito de compras</h1>
-        <form method="post" action="https://hydronaut.mx/api/pay" className="mt-12 lg:grid lg:grid-cols-12 lg:items-start lg:gap-x-12 xl:gap-x-16">
+        <form method="post" action="/api/pay" className="mt-12 lg:grid lg:grid-cols-12 lg:items-start lg:gap-x-12 xl:gap-x-16">
           <input type="hidden" value={cart.length} name="length" />
           <section aria-labelledby="cart-heading" className="lg:col-span-7">
             <h2 id="cart-heading" className="sr-only">
